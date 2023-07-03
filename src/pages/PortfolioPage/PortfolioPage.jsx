b9a4ba428d3710ae0509013fe759bb3fc8705e84
Andrey <andrey.kuh96@gmail.com>
2023-07-03 22:19:40 +0300

    fixed link one more)

--- src/pages/PortfolioPage/PortfolioPage.jsx
+++ src/pages/PortfolioPage/PortfolioPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './PortfolioPage.scss';
 import './PortfolioPage.media.scss';
 import Text from '../../components/Text/Text';
+import { Link } from 'react-router-dom';
 
 const PortfolioPage = () => {
 	return (
@@ -15,60 +16,60 @@ const PortfolioPage = () => {
 					title={'Портфолио'}
 				/>
 				<div className="portfolioPage__wrapper">
-					<a
-						href="https://andreykuh96.github.io/employsitytest/"
-						className="portfolioPage__link portfolioPage__link_1"
-						target="_blank"
-						rel="noopener noreferrer"
-					></a>
-					<a
-						href="https://andreykuh96.github.io/SpaceShop/"
-						className="portfolioPage__link portfolioPage__link_2"
-						target="_blank"
-						rel="noopener noreferrer"
-					></a>
-					<a
-						href="https://andreykuh96.github.io/marvel/"
-						className="portfolioPage__link portfolioPage__link_3"
-						target="_blank"
-						rel="noopener noreferrer"
-					></a>
-					<a
-						href="https://andreykuh96.github.io/HolaFashion/"
+					<Link
+						to="https://andreykuh96.github.io/HolaFashion/"
 						className="portfolioPage__link portfolioPage__link_4"
 						target="_blank"
 						rel="noopener noreferrer"
-					></a>
-					<a
-						href="https://andreykuh96.github.io/Flodesk/"
-						className="portfolioPage__link portfolioPage__link_5"
-						target="_blank"
-						rel="noopener noreferrer"
-					></a>
-					<a
-						href="https://andreykuh96.github.io/explorer/"
+					></Link>
+					<Link
+						to="https://andreykuh96.github.io/explorer/"
 						className="portfolioPage__link portfolioPage__link_6"
 						target="_blank"
 						rel="noopener noreferrer"
-					></a>
-					<a
-						href="https://andreykuh96.github.io/coffehouse/"
+					></Link>
+					<Link
+						to="https://andreykuh96.github.io/coffehouse/"
 						className="portfolioPage__link portfolioPage__link_7"
 						target="_blank"
 						rel="noopener noreferrer"
-					></a>
-					<a
-						href="https://andreykuh96.github.io/projectReactHooks/"
+					></Link>
+					<Link
+						to="https://andreykuh96.github.io/employsitytest/"
+						className="portfolioPage__link portfolioPage__link_1"
+						target="_blank"
+						rel="noopener noreferrer"
+					></Link>
+					<Link
+						to="https://andreykuh96.github.io/Flodesk/"
+						className="portfolioPage__link portfolioPage__link_5"
+						target="_blank"
+						rel="noopener noreferrer"
+					></Link>
+					<Link
+						to="https://andreykuh96.github.io/SpaceShop/"
+						className="portfolioPage__link portfolioPage__link_2"
+						target="_blank"
+						rel="noopener noreferrer"
+					></Link>
+					<Link
+						to="https://andreykuh96.github.io/marvel/"
+						className="portfolioPage__link portfolioPage__link_3"
+						target="_blank"
+						rel="noopener noreferrer"
+					></Link>
+					<Link
+						to="https://andreykuh96.github.io/projectReactHooks/"
 						className="portfolioPage__link portfolioPage__link_8"
 						target="_blank"
 						rel="noopener noreferrer"
-					></a>
-					<a
-						href="https://andreykuh96.github.io/projectReact/"
+					></Link>
+					<Link
+						to="https://andreykuh96.github.io/projectReact/"
 						className="portfolioPage__link portfolioPage__link_9"
 						target="_blank"
 						rel="noopener noreferrer"
-					></a>
+					></Link>
 				</div>
 			</div>
 		</div>
