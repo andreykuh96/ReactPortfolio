57a349b29e83a7eb99caa2872ffe94204f9cd3e9
Andrey <andrey.kuh96@gmail.com>
2023-07-04 11:38:12 +0300

    contacts page

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,8 +1,54 @@
 import React from 'react';
 import './ContactsPage.scss';
+import './ContactsPage.media.scss';
+import img_call from '../../img/call.png';
+import Text from '../../components/Text/Text';
+import { Link } from 'react-router-dom';
 
 const ContactsPage = () => {
-	return <div>ContactsPage</div>;
+	return (
+		<div className="contacts">
+			<div className="container">
+				<Text
+					title={'Контакты'}
+					subtitle={'Свяжитесь со мной'}
+					fz36={true}
+					line={true}
+				/>
+				<div className="contacts__img">
+					<Link
+						to="https://t.me/andreykuhtov"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<span className="contacts__icon contacts__icon_telegram icon-telegram"></span>
+					</Link>
+					<Link
+						to="https://www.instagram.com/andrey.kuh/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<span className="contacts__icon contacts__icon_instagram icon-instagram"></span>
+					</Link>
+					<Link
+						to="https://github.com/andreykuh96"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<span className="contacts__icon contacts__icon_github icon-github"></span>
+					</Link>
+					<Link
+						to="https://vk.com/andrey_kuh"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<span className="contacts__icon contacts__icon_vk icon-vk"></span>
+					</Link>
+					<img src={img_call} alt="call me" />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ContactsPage;
